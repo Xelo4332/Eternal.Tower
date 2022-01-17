38316a35d7499a971693bb4deb5883109166639b
Coding-boot <kacper.kijewski@elev.ga.lbs.se>
2022-01-17 15:26:18 +0100

    tf

---
+++ Assets/RangedEnemy.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    // Parameters for shit
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [Header("Ranged Attack")]
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private GameObject[] fireballs;
+
+
+   [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("PlayerLayer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    //references remove the slashes when animation is done
+    // private Animator anim;
+    // private EnemyPatrol enemyPatrol;
+
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+         
+        //attack only if player in sight
+        if(PlayerInSight())
+        {
+            if(cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("rangedAttack");
+            }
+         }
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
+    }
+
+    private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        //shootii projectile 
+        fireballs[0].transform.position = firepoint.position;
+        fireballs[0].GetComponent<EnemyProjectile>().ActiveProjectile();
+    }
+
+    private int FindFireball()
+    {
+       for (int i = 0; i < fireballs.Length; i++)
+        {
+            if(!fireballs[i]).activeInHierarchy)
+                    return i;
+        }
+        return 0;
+
+    }
+
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+}
+
+
+
+}
