f6aa75a362836646c57f698fcf6e2d7fdb27295e
Coding-boot <kacper.kijewski@elev.ga.lbs.se>
2022-01-17 13:19:38 +0100

    LOLOLOLOLOLOLOLOLOLOLOLOLOLOOOLLLLOOOLO

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -16,23 +16,23 @@ public class PlayerMove : MonoBehaviour
 
     public Transform attackPoint;
     public float attackRange = 0.5f;
-    public int attackDamage = 40;
+    public int attackDamage = 1;
     private float timer;
     private bool parry;
     [SerializeField] private GameObject parryBox;
     private float parryWindow = 0.5f;
+    public int maxhealth = 3;
+    int currenthealth;
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = transform.GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
         parryBox.SetActive(false);
+        currenthealth = maxhealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -40,40 +40,40 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
-            transform.position += new Vector3(speed, 0, 0)* Time.deltaTime;// These "If" codes make so that you move the diraction u press the button of
+            transform.position += new Vector3(speed, 0, 0)* Time.deltaTime;// These "If" codes make so that you move the diraction u press the button of-KK
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
             transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
         }
-        if (IsGrounded() && Input.GetKey(KeyCode.W)) // This code adds force upwards to the player making it jump
+        if (IsGrounded() && Input.GetKey(KeyCode.W)) // This code adds force upwards to the player making it jump -KK
         {
             
             float jumpforce = 15f;
             rb.velocity = Vector2.up * jumpforce; 
 
         }
-        if (Input.GetKey(KeyCode.Space)& timer>1f)// press space to attack
+        if (Input.GetKey(KeyCode.Space)& timer>1f)// press space to attack-KK
         {
             Attack();
             timer = 0;
         }
-        if (Input.GetKey(KeyCode.K)&& !parry)
+        if (Input.GetKey(KeyCode.P)&& !parry)// if you press the p key the this kode activates a emptybox that funcions as the parry box...-KK
         {
             parryBox.SetActive(true);
                 parry= true;
             Invoke("ParryStop", parryWindow);
         }
     }
-    void ParryStop()
+    void ParryStop()// stops the parry window-KK
     {
         parryBox.SetActive(false);
         parry = false;
     }
     void Attack()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); // 
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); // Looks if the enemy is in the empty circle and gives it damage-KK
 
         foreach (Collider2D enemy in hitEnemies)
         {
@@ -83,14 +83,14 @@ public class PlayerMove : MonoBehaviour
     }
 
 
-private bool IsGrounded() // this code makes a boxcast thats checks if the player is touching the ground so that u can only jump on the ground
+private bool IsGrounded() // this code makes a boxcast thats checks if the player is touching the ground so that u can only jump on the ground-KK
     {
        
        RaycastHit2D raycasthit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down,  .1f, platformslayermask);
         Debug.Log(raycasthit2d.collider);
         return raycasthit2d.collider != null;
     }
-    private void OnDrawGizmosSelected()// shows the empty circle
+    private void OnDrawGizmosSelected()// shows the empty circle that is the attack range-KK
     {
         if (attackPoint == null)
             return;
