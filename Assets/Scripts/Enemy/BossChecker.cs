16de258ef7757e876a48bba6dee50ac73d9cfad8
Xelo4332 <mansor-deni.mamergov@elev.ga.lbs.se>
2022-02-17 13:40:18 +0100

    LAST CHANGE

---
+++ Assets/Scripts/Enemy/BossChecker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossChecker : MonoBehaviour
+{
+    //Skappar array och health variable 
+    EnemyHealth[] enemies;
+    private Health _health;
+    void Start()
+    {
+        enemies = GameObject.FindObjectsOfType<EnemyHealth>();
+    }
+
+    //Om alla enemies lika med deadm då kommer loada andra scene Deni
+    void Update()
+    {
+        if (checkEnemies())
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
+
+    //Kollar om alla enemis är lika med dead Deni
+    bool checkEnemies()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i].dead == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
